bef90576469afab78c63e0d726833ebaecaa0315
Thalys Márcio <thalys@live.com>
2024-05-14 22:39:55 -0300

    phpunit 10.0

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class ExampleTest extends BaseTestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
